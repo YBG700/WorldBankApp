0220bcb4bf6f3554142a75e0dde7a7f8e873a5b6
RebelSnowStorm <danfirestarter@gmail.com>
2024-03-11 15:54:28 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WorldBankApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
