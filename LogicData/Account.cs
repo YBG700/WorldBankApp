dfac175b5b99b27fb04d02874fb0a5a94e23f52b
RebelSnowStorm <danfirestarter@gmail.com>
2024-03-13 13:52:39 -0400

    Created Account, SavingsAccount, and ChequingAccount

--- LogicData/Account.cs
+++ LogicData/Account.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorldBankApp.LogicData
+﻿namespace WorldBankApp.LogicData
 {
-    internal class Account
+    class Account
     {
+        // Standard Account
+        private int _accNum;
+        private string _accHolderName;
+        private int _accPhoneNum;
+        private string _accEmail;
+        private int _currBal;
+
+        // Constructor
+        public Account(int accNum, string holderName, int phoneNum, string email)
+        {
+            _accNum = accNum;
+            _accHolderName = holderName;
+            _accPhoneNum = phoneNum;
+            _accEmail = email;
+            _currBal = 0;
+        }
+    }
+
+    class SavingsAccount : Account
+    {
+        // Savings Account
+        private int _minBal;
+
+        // Constructor
+        public SavingsAccount(int minBal, int accNum, string holderName, int phoneNum, string email)
+            : base(accNum, holderName, phoneNum, email)
+        {
+            _minBal = minBal;
+        }
+    }
+
+    class ChequingAccount : Account
+    {
+        // Chequing Account
+        private int _overDraftLimit;
+
+        // Constructor
+        public ChequingAccount(int overDraftLimit, int accNum, string holderName, int phoneNum, string email)
+            : base(accNum, holderName, phoneNum, email)
+        {
+            _overDraftLimit = overDraftLimit;
+        }
     }
 }
