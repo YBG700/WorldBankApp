1659463d6f9698e81b92c02e8e7807766a182acb
YBG700 <obasekigabriel700@gmail.com>
2024-03-14 19:30:50 -0400

    Set up page Transitions

--- SearchAcc.xaml.cs
+++ SearchAcc.xaml.cs
@@ -1,9 +1,15 @@
-namespace WorldBankApp;
-
-public partial class SearchAcc : ContentPage
+namespace WorldBankApp
 {
-	public SearchAcc()
-	{
-		InitializeComponent();
-	}
+    public partial class SearchAcc : ContentPage
+    {
+        public SearchAcc()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnBackClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync(); // Use PopAsync to navigate back to the previous page
+        }
+    }
 }
