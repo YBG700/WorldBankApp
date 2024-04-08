3715bd9ec3a71b90d054e0428d4c3f185ac43cae
RebelSnowStorm <danfirestarter@gmail.com>
2024-04-08 19:31:43 -0400

    Created Deal Constructor, Created enum DealEnum

--- LogicData/Deals.cs
+++ LogicData/Deals.cs
@@ -1,15 +1,25 @@
 ﻿namespace WorldBankApp.LogicData
 {
+    public enum DealEnum
+    {
+        StudentSavings,
+        CreditPlus,
+        Interest555
+    }
+
     public class Deals
     {
         public string dealName;
         public string dealDesc;
         public int dealLength;
-        public int dealType;
+        public DealEnum type;
 
-        public Deals()
+        public Deals(string name, string desc, int length, DealEnum dealType)
         {
-            
+            dealName = name;
+            dealDesc = desc;
+            dealLength = length;
+            type = dealType;
         }
     }
 }
